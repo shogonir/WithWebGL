239ac86c82366eee04dadb071a140a2442b5af07
shogonir <shogonir@gmail.com>
2016-08-15 23:13:11 +0900

    [modify] display white triangle

--- second/second.js
+++ second/second.js
@@ -11,10 +11,61 @@ onload = function() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    vshader = create_shader('vertex-shader');
-    fshader = create_shader('fragment-shader');
+    var vshader = create_shader('vertex-shader');
+    var fshader = create_shader('fragment-shader');
 
-    create_program(vshader, fshader);
+    var prg = create_program(vshader, fshader);
+
+    var attLocation = gl.getAttribLocation(prg, 'position');
+
+    var attStride = 3;
+
+    var vertex_position = [
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        -1.0, 0.0, 0.0
+    ];
+
+    var vbo = create_vbo(vertex_position);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+    gl.enableVertexAttribArray(attLocation);
+
+    gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+
+    var mMat = identity4x4();
+    
+    var vMat = makeLookAt(
+        0.0, 1.0, 3.0,
+        0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0
+    );
+
+    var pMat = makePerspective(90.0, 1.0 * c.width / c.height, 0.1, 100);
+
+    var mvMat = vMat.x(mMat);
+
+    var mvpMat = pMat.x(mvMat);
+
+    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+
+    console.log(mvpMat);
+
+    gl.uniformMatrix4fv(uniLocation, false, mvpMat.flatten());
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+    gl.flush();
+}
+
+function identity4x4 () {
+    return $M([
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ]);
 }
 
 function create_shader (id) {
@@ -67,3 +118,12 @@ function create_program(vs, fs){
         alert(gl.getProgramInfoLog(program));
     }
 }
+
+function create_vbo (data) {
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    return vbo;
+}
+
